7abd08f95620b8bec6a540a01e7482f1ba5b8917
x0byte <hirunwe@gmail.com>
2024-11-17 02:18:25 +0530

    Added the interactive world map of the circuits

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import requests
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+import folium
 
 app = Flask(__name__)
 
@@ -15,7 +16,62 @@ def calender():
     response = requests.get(url)
     data = response.json()
     races = data['MRData']['RaceTable']['Races']
-    return render_template('calender.html', races=races, date=date, datetime=datetime)
+
+    # Find the next race
+    today = datetime.today().date()
+    next_race = None
+    for race in races:
+        race_date = datetime.strptime(race["date"], "%Y-%m-%d").date()
+        if race_date >= today:
+            if not next_race or race_date < datetime.strptime(next_race["date"], "%Y-%m-%d").date():
+                next_race = race
+
+
+    #next race time convert
+    utc_time = datetime.strptime(next_race['time'], "%H:%M:%SZ")
+    offset = timedelta(hours=5, minutes=30)
+
+    local_time = utc_time + offset
+
+    tracks = []
+    for race in races:
+        track = {
+            "name": race['Circuit']['circuitName'],
+            "location": f"{race['Circuit']['Location']['locality']}, {race['Circuit']['Location']['country']}",
+            "latitude": float(race['Circuit']['Location']['lat']),
+            "longitude": float(race['Circuit']['Location']['long'])
+        }
+        tracks.append(track)
+
+    # Initialize the map centered globally
+    world_map = folium.Map(location=[20, 0], zoom_start=2, tiles=None)
+
+    folium.TileLayer(
+    tiles="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+    attr="Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ",
+    name="ESRI Satellite"
+    ).add_to(world_map)
+
+    folium.LayerControl().add_to(world_map)
+
+
+    # Add markers for each track
+    for track in tracks:
+        folium.Marker(
+            location=[track["latitude"], track["longitude"]],
+            popup=f"<b>{track['name']}</b><br>{track['location']}",
+            icon=folium.CustomIcon('static/assets/location-icon.png', icon_size=(30, 30))
+        ).add_to(world_map)
+
+    
+
+    # Save the map as an HTML object
+    map_html = world_map._repr_html_()  # Render the map as HTML
+
+
+    return render_template('calender.html', races=races, date=date, datetime=datetime, next_race=next_race, local_time=local_time.time(), map_html=map_html)
+
+
 
 
 @app.route('/race/<round>')
